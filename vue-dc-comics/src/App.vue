d57bbd7e2b718805497d93c765792f68a1e265d1
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-07-07 15:19:19 +0200

    add: header

--- vue-dc-comics/src/App.vue
+++ vue-dc-comics/src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header/>
+
+    <main>
+      <!-- <Content/> -->
+      <!-- <Shop/> -->
+    </main>
+
+    <!-- <Footer/> -->
+
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+// import Content from "./components/Content.vue";
+// import Shop from "./components/Shop.vue";
+// import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header
+    // Content,
+    // Shop,
+    // Footer
   },
 };
 </script>
@@ -21,8 +34,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // text-align: center;
+  // color: #2c3e50;
+  // margin-top: 60px;
 }
 </style>
---
+++ vue-dc-comics/src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <header>
+      <a href="#">
+          <img src="../assets/img/dc-logo.png" alt="dc logo">
+      </a>
+      <nav>
+          <ul>
+              <li><a href="#">characters</a></li>
+              <li><a href="#">comics</a></li>
+              <li><a href="#">movies</a></li>
+              <li><a href="#">tv</a></li>
+              <li><a href="#">games</a></li>
+              <li><a href="#">collectibles</a></li>
+              <li><a href="#">videos</a></li>
+              <li><a href="#">fans</a></li>
+              <li><a href="#">news</a></li>
+              <li><a href="#">shop</a></li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+header{
+    margin: 0 10%;
+    padding-top: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100px;
+    a{
+
+    }
+    ul{
+        li{
+            list-style: none;
+            display: inline;
+            margin: 0 10px;
+            font-size: 15px;
+            
+            a{
+                text-decoration: none;
+                text-transform: uppercase;
+                color: #000;
+                padding: 40px 0;
+                &:hover{
+                    border-bottom: 3px solid blue;
+                    color: blue;
+                }
+            }
+        }
+    }
+}
+
+</style>
